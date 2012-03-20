2a68d902dbafe3e8ed429887f3547f7a01358ee5
Nicholas Romeo <nicholas@digitalwake.com>
2012-03-20 17:28:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thummed::Application.config.secret_token = 'b457326d1e69bd8d51fc323538d85b27c6b97e2628a7678ece4b61e27c0caa079ef750daa886e51efa6e7244667952d44f923ab22411942fda02effcc72bdc9c'
